4f4b31409efa985cd69b6a2e04a70286c19b59ba
HoRizon <jiatenghuipl@gmail.com>
2022-04-20 14:56:14 +0800

    1.0

--- save_as_csv.py
+++ save_as_csv.py
@@ -6,5 +6,9 @@ import pandas as pd
 # sheet = pd.read_excel(io="./2018/巴南.xls", header=None)
 # print(sheet)
 
-data_xls = pd.read_excel('渝北.xls', sheet_name='sheet1')
-data_xls.to_csv('渝北.csv', encoding='utf-8', index=False)
+path = './test/'
+file_list = os.listdir(path)
+for i in file_list:
+    save_name = i.replace("xls", "csv")
+    data_xls = pd.read_excel(path + i, sheet_name='sheet1')
+    data_xls.to_csv(path + save_name, encoding='utf-8', index=False)
